868d589ebaa61005628197a61694153917a9873f
czM1K3 <misamadera1@gmail.com>
2021-01-25 18:03:27 +0100

    Added UTF-8 support

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "Calculator.h"
+#include <windows.h>
 
 using namespace std;
 
 int main(){
+    SetConsoleOutputCP(CP_UTF8);
     Calculator calc;
     calc.Calculate("bum");
     
