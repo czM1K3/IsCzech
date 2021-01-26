37ef8e9a67c337e6df64ea6cd2af202d9fcb2af7
czM1K3 <misamadera1@gmail.com>
2021-01-26 12:09:08 +0100

    Fixed linux support

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
-#include <windows.h>
 #include <fstream>
 #include "Calculator.h"
 
+#ifdef WIN32
+#include <windows.h>
+#endif
+
 using namespace std;
 
-int main(){
+int main()
+{
+#ifdef WIN32
     SetConsoleOutputCP(CP_UTF8);
+#endif
     Calculator calc;
 
     ifstream file("input.txt");
@@ -15,7 +21,6 @@ int main(){
     {
         calc.Calculate(str);
     }
-    
 
     return 0;
 }
